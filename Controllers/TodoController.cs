fc7f098bb8dfc1d04fde6476d65dbc8a49ef003c
ditiz <mathieu.smarzyk@campus-numerique-in-the-alps.com>
2019-05-08 19:23:52 +0200

    add put and delete method

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -49,5 +49,33 @@ namespace sharp_api.Controllers
 
 			return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
 		}
+
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutTodoItem(long id, TodoItem item)
+		{
+			if (id != item.Id) {
+				return BadRequest();
+			}
+
+			_context.Entry(item).State = EntityState.Modified;
+			await _context.SaveChangesAsync();
+
+			return NoContent();
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteTodoItem(long id)
+		{
+			var todoItem = await _context.TodoItems.FindAsync(id);
+
+			if (todoItem == null) {
+				return NotFound();
+			}
+
+			_context.TodoItems.Remove(todoItem);
+			await _context.SaveChangesAsync();
+
+			return NoContent();
+		}
 	}
 }
